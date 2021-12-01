6af5f2fc0dc269142304971f51b56bb9bb282c31
Jun Li <29033600+JunLi-Galios@users.noreply.github.com>
2021-11-30 17:12:31 -0800

    Create Extremeness.py

---
+++ Extremeness.py
@@ -0,0 +1,25 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+class Extremeness(Object):
+    def __init__(self):
+        super(Extremeness，self).__init__()
+    
+    def cal_extreme(inp):
+        raise NotImplementedError
+        
+    def cal_extreme(inp):
+        raise NotImplementedError
+        
+        
+class AvgExtremeness(Extremeness):
+    def __init__(self):
+        super(AvgExtremeness，self).__init__()
+        
+    def cal_extreme(inp): 
+    
+    
