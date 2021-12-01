ecb07dc22f1b49f471c6e21da6ca49cea7d97bd4
Jun Li <29033600+JunLi-Galios@users.noreply.github.com>
2021-12-01 15:25:43 -0800

    Update Extremeness.py

--- Extremeness.py
+++ Extremeness.py
@@ -6,20 +6,61 @@ from torch.autograd import Variable
 import numpy as np
 
 class Extremeness(Object):
-    def __init__(self):
+    def __init__(self, mu):
         super(Extremeness，self).__init__()
+        self.mu = mu
     
-    def cal_extreme(inp):
+    def cal_extreme(self, inp):
         raise NotImplementedError
         
-    def cal_extreme(inp):
+    def func(self):
         raise NotImplementedError
+        
+    def optimize(self, inp_size):        
+        X = torch.nn.Parameter(torch.randn(inp_size) * 0.001)
+        fn = self.func()
+        loss = - fn(X)
+        optimizer = optim.SGD(X, lr=0.001)
+        while loss > -self.mu:
+            optimizer.zero_grad()
+            lossG.backward()
+            optimizer.step()
+            loss = - fn(X)
+            
+        return X.data
         
         
 class AvgExtremeness(Extremeness):
-    def __init__(self):
-        super(AvgExtremeness，self).__init__()
+    def __init__(self, mu):
+        super(AvgExtremeness，self).__init__(self, mu)
+        print('AvgExtremeness mu', mu)
         
-    def cal_extreme(inp): 
+    def cal_extreme(self, inp):
+        batch = inp.size()[0]
+        re_inp = inp.reshape(batch, -1)
+        return torch.mean(re_inp, dim=1)
+    
+    def func(self):
+        fn = lambda x: torch.mean(x)
+        return fn
+    
+    
+    
+class MaxExtremeness(Extremeness):
+    def __init__(self,mu):
+        super(MaxExtremeness，self).__init__(self, mu)
+        print('MaxExtremeness mu', mu)
+        
+    def cal_extreme(inp):
+        batch = inp.size()[0]
+        re_inp = inp.reshape(batch, -1)
+        return torch.max(re_inp, dim=1).values
+    
+    def func(self):
+        fn = lambda x: torch.max(x)
+        return fn
+    
+    
+
     
     
